d8cb6e4cb8a3b9b0147634c2ce121c427f3ce716
streepje8 <wessel@wezzel.nl>
2022-11-17 15:13:19 +0100

    Epic changes

--- Assets/Scripts/Astar.cs
+++ Assets/Scripts/Astar.cs
@@ -22,11 +22,16 @@ public class Astar
         };
         while (openNodes.Count > 0 && loops < maxLoops)
         {
+            //For stats and to prevent an infintie loop
             loops++;
-            openNodes = openNodes.OrderByDescending(x => -x.FScore).ToList();
+            
+            //Ordering and preperation
+            openNodes = openNodes.OrderBy(x => x.FScore).ToList();
             Node current = openNodes[0];
             openNodes.Remove(current);
             closedNodes.Add(current);
+            
+            //Is our goal reached?
             if (current.position == endPos)
             {
                 List<Vector2Int> path = new List<Vector2Int>();
@@ -36,26 +41,36 @@ public class Astar
                     path.Add(currentBack.position);
                     currentBack = currentBack.parent;
                 }
-                Debug.Log("POG in " + loops + " loops!");
+                Debug.Log("Objective found in " + loops + " loops!");
                 path.Reverse();
                 return path;
             }
+            
+            //Get adjacent nodes of current node
             Cell currentCell = grid[current.position.x,current.position.y];
             List<Cell> neighbors = currentCell.GetNeighbours(grid);
             List<Node> adjacent = new List<Node>();
             foreach (var neighbor in neighbors)
                 if (!currentCell.HasWall(directionToDirection[neighbor.gridPosition - currentCell.gridPosition]))
                     adjacent.Add(new Node(neighbor.gridPosition, current, 0, 0));
+            
+            //Process those nodes
             foreach (var node in adjacent)
             {
+                //Node already visited
                 if (closedNodes.Any(x => x.EqualTo(node)))
                     continue;
+                
+                //Calculate the scores
                 node.GScore = current.GScore + Vector2Int.Distance(node.position, current.position);
                 node.HScore = Vector2Int.Distance(node.position, endPos);
+                
+                //If the node is already open
                 if (openNodes.Any(x => x.EqualTo(node)))
                 {
+                    //Find the one in the list
                     Node inList = openNodes.Where(x => x.EqualTo(node)).ToList()[0];
-                    if (inList.GScore < node.GScore)
+                    if (inList.GScore < node.GScore) //See if the new one is faster
                     {
                         openNodes.Remove(inList);
                         openNodes.Add(node);
@@ -63,6 +78,7 @@ public class Astar
                 }
                 else
                 {
+                    //Add it to the unvisited nodes to process
                     openNodes.Add(node);
                 }
             }
